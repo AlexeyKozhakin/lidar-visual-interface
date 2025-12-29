bddb5720a097f50a508938fbc1d94d2935f97a05
alexe <alexeykozhakin@yandex.ru>
2025-12-29 22:49:06 +0100

    fix in config file path to model

--- musac_las_classifier/predictor_multiclass_segmentation/config_prediction.py
+++ musac_las_classifier/predictor_multiclass_segmentation/config_prediction.py
@@ -1,7 +1,8 @@
-    
 # === prediction settings
+from importlib.resources import files
 
-checkpoint_path = "musac_las_classifier/predictor_multiclass_segmentation/model/model_epoch_31.pth"
-encoder_weights_path = "musac_las_classifier/predictor_multiclass_segmentation/model/resnet34-333f7ec4.pth"
+
+checkpoint_path = files("musac_las_classifier.predictor_multiclass_segmentation") / "model/model_epoch_31.pth"
+encoder_weights_path =  files("musac_las_classifier.predictor_multiclass_segmentation") / "model/resnet34-333f7ec4.pth"
 output_directory = r'example_data/img_predict_multicalss'
 output_directory_join = "temp/img_predict_multi_class_join"
